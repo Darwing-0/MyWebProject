91933ffaec0862a69a2ac82a90312908e90c618a
Darwing-0 <darwing.delva@gmail.com>
2024-04-01 08:50:40 -0400

    updated the schema of sql

--- database/dbSetup.sql
+++ database/dbSetup.sql
@@ -10,7 +10,7 @@ age INT,
 sex VARCHAR(100),
 goals VARCHAR(100),
 constraint user_pk primary key(userID)
-)
+);
 
 CREATE TABLE GOALS(
 goalID int auto_increment,
@@ -18,7 +18,7 @@ userID int NOT NULL,
 goalType ENUM('lose weight', 'maintain weight', 'gain weight') NOT NULL,
 constraint goals_PK primary key(goalID),
 constraint user_goals_fk foreign key(userID) references USERS(userID)
-)
+);
 
 CREATE TABLE NUTRITIONPLANS(
 planID int auto_increment,
@@ -29,7 +29,7 @@ carbRatio int NOT NULL,
 fatRatio int NOT NULL,
 constraint nutrition_fk primary key(planID),
 constraint user_nutrition_fk foreign key(userID) references USERS(userID)
-)
+);
 
 CREATE TABLE WORKOUTROUTINE(
 routineID int auto_increment,
@@ -39,4 +39,20 @@ description varchar(500) NOT NULL,
 duration int,
 constraint routine_fk primary key(routineID),
 constraint goal_routine_fk foreign key(goalID) references GOALS(goalID)
-)
+);
+
+CREATE TABLE comments(
+id INT AUTO_INCREMENT PRIMARY KEY,
+post_id INT,
+content TEXT,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (post_id) REFERENCES  posts(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE posts (
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ title VARCHAR(255),
+ content TEXT,
+ created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
